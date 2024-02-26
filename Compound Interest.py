600f4f32662a7ea5187b1720d7df057c6b8e9aa6
caugnon <joffreycaugnon@gmail.com>
2024-02-26 16:10:21 +0100

    communication with user in console

--- Compound Interest.py
+++ Compound Interest.py
@@ -5,5 +5,15 @@ def compoundInterest(capital, apy, paymentDuration, years):
     while eachPayment < totalPayment:
         capital += (capital * apy) / paymentDuration
         eachPayment += 1
+    print(capital)
     return capital
 
+
+# Demander à l'utilisateur d'entrer les valeurs nécessaires
+capital = float(input("Entrez le capital initial : "))
+apy = float(input("Entrez le taux d'intérêt annuel (sous forme décimale) : "))
+paymentDuration = int(input("Entrez la durée du paiement (annuel: 1, mensuel: 12, hebdomadaire: 52, journalier: 365) : "))
+years = int(input("Entrez le nombre d'années : "))
+
+# Appel de la fonction
+compoundInterest(capital, apy, paymentDuration, years)
