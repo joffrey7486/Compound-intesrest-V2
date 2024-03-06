c7a7f1e6e132375ddfb6ca27891e5b0e6d7ac1ac
caugnon <joffreycaugnon@gmail.com>
2024-03-06 12:11:54 +0100

    add regular fluctuation

--- Compound Interest.py
+++ Compound Interest.py
@@ -2,18 +2,21 @@
 capital = float(input("Entrez le capital initial : "))
 reInvestment = float(input("Entrez combien vous voulez réinvestir chaque mois (0 si nul): "))
 apy = float(input("Entrez le taux d'intérêt annuel (sous forme décimale) : "))
+fluctuation = float(input("Entrez la fluctuation annuelle en % (sous forme décimale) : "))
 paymentDuration = int(input("Entrez la durée du paiement (annuel: 1, mensuel: 12, hebdomadaire: 52, journalier: 365) : "))
 years = int(input("Entrez le nombre d'années : "))
 
 # Capital => initial number, apy => 8% => 0.08, paymentDuration => every day 365 every week 52 every months 12, year => total number years invest
-def compoundInterest(capital, reInvestment, apy, paymentDuration, years):
+def compoundInterest(capital, reInvestment, apy, fluctuation, paymentDuration, years):
     totalDays = years * 365
+    monthlyFluctuation = fluctuation / 12
     for day in range(totalDays):
         if day % paymentDuration == 0:
             interest = (capital * apy) / (365 / paymentDuration)
             capital += interest
         if day % 30 == 0:
             capital += reInvestment
+            capital *= (1 + monthlyFluctuation)
 
     print("Voici votre capital au bout de {} ans: {}".format(years, capital))
     return capital
@@ -23,4 +26,4 @@ def compoundInterest(capital, reInvestment, apy, paymentDuration, years):
 
 
 # Appel de la fonction
-compoundInterest(capital, reInvestment, apy, paymentDuration, years)
+compoundInterest(capital, reInvestment, apy, fluctuation, paymentDuration, years)
